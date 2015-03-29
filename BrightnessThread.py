f9fb18690bb2cf07dd1fed0b51110ca8cd2abc1b
Joel Roberts <joel@joelroberts.net>
2015-03-29 20:49:56 +0000

    enable brightness control

--- BrightnessThread.py
+++ BrightnessThread.py
@@ -88,10 +88,10 @@ class BrightnessThread(threading.Thread):
          levelDiff = abs(self.currentLevel - avgLevel)
 
          #log.debug("Reading: %s, Percentage: %s, NewLevel: %s, AvgLevel: %s, Diff: %s" % (reading,percentage,newLevel,avgLevel,levelDiff))
-         log.debug("Reading: %s, NewLevel: %s, AvgLevel: %s, Diff: %s" % (reading,newLevel,avgLevel,levelDiff))
+         #log.debug("Reading: %s, NewLevel: %s, AvgLevel: %s, Diff: %s" % (reading,newLevel,avgLevel,levelDiff))
 
          if(levelDiff>=2):
-            log.debug("Updating brightness to %s" % (avgLevel))
+            #log.debug("Updating brightness to %s" % (avgLevel))
             self.currentLevel=avgLevel
             self.updateBrightness()
 
