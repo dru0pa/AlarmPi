92d25706dd80d16cc38e34fc6dd076c3d1744320
Joel Roberts <joel@joelroberts.net>
2015-04-02 22:17:58 +0000

    replace sqlite with json

--- Settings.py
+++ Settings.py
@@ -354,7 +354,7 @@ class Settings:
 
 
     def get(self, key):
-        print "SEttings: " + self.settings
+        print "SEttings: " + json.dumps(self.settings)
         r = self.settings[0][key]["value"]
         if r is None:
             raise Exception('Could not find setting %s' % (key))
