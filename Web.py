11380db80b4d11ab3ebc8a7a18bdf4bc07efb056
Joel Roberts <joel@joelroberts.net>
2015-04-03 20:48:56 +0000

    working on dynamic forms

--- Web.py
+++ Web.py
@@ -98,7 +98,7 @@ class set:
             else:
                 nullArg = ()
             if dict["formType"] == 'textbox':
-                dynamic_form.add_input(form.Textbox(dict["key"], form.regexp(dict["formRegexp"], dict["formRegexpMessage"]), description=dict["description"],
+                dynamic_form.add_input(form.Textbox(dict["key"], description=dict["description"],
                                  value=dict["value"], *nullArg))
             elif dict["formType"] == 'radio':
                 dynamic_form.add_input(form.Radio(dict["key"], args=dict["formDropdownValues"], description=dict["description"],
