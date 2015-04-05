440b9339f9dc3e3f15d7a00fe345e6e7a5c68a19
Joel Roberts <joel@joelroberts.net>
2015-04-05 16:24:26 +0000

    updated default wink_group_id

--- Settings.py
+++ Settings.py
@@ -345,7 +345,7 @@ class Settings:
             "wink_group_id": {
                 "formOrder": 47,
                 "key": "wink_group_id",
-                "value": "85",
+                "value": "2901669",
                 "description": "Wink Group ID to manipulate",
                 "formType": "textbox",
                 "visibility": "standard",
