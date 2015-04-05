59320cd60a13177f391926aabace8ab3eedc8200
Joel Roberts <joel@joelroberts.net>
2015-04-05 02:27:37 +0000

    check-in Wink.py

---
+++ Wink.py
@@ -0,0 +1,86 @@
+from urllib2 import Request, urlopen
+import json
+
+api_server = "https://winkapi.quirky.com"
+#api_server = "https://private-0d714-wink.apiary-mock.com"
+
+values = json.dumps({
+    "client_id": "9575057b69283d441c17f3618d15d93d",
+    "client_secret": "4aaf8ea1c1064166e3df8bae3e109598",
+    "username": "joel@joelroberts.net",
+    "password": "p@ssw0rd",
+    "grant_type": "password"
+})
+headers = {"Content-Type": "application/json"}
+request = Request("{0}/oauth2/token".format(api_server), data=values, headers=headers)
+response_body = urlopen(request).read()
+oauth2 = json.loads(response_body)
+print json.dumps(oauth2, sort_keys=True, indent=4, separators=(',', ': '))
+
+# headers = {"Authorization": "Bearer {0}".format(oauth2["data"]["access_token"])}
+# request = Request("{0}/users/me/light_bulbs".format(api_server), headers=headers)
+# response_body = urlopen(request).read()
+# light_bulbs = json.loads(response_body)
+# print json.dumps(light_bulbs, sort_keys=True, indent=4, separators=(',', ': '))
+#
+# headers = {"Authorization": "Bearer {0}".format(oauth2["data"]["access_token"])}
+# request = Request("{0}/users/me/wink_devices".format(api_server), headers=headers)
+# response_body = urlopen(request).read()
+# wink_devices = json.loads(response_body)
+# print json.dumps(wink_devices, sort_keys=True, indent=4, separators=(',', ': '))
+
+# new_group = {
+#     "name": "Bedroom",
+#     "order": 3,
+#     "members": [
+#         {
+#             "object_id": "467288",
+#             "object_type": "light_bulb"
+#         },
+#         {
+#             "object_id": "467290",
+#             "object_type": "light_bulb"
+#         },
+#         {
+#             "object_id": "467302",
+#             "object_type": "light_bulb"
+#         }
+#     ]
+# }
+#
+# headers = {"Authorization": "Bearer {0}".format(oauth2["data"]["access_token"])}
+# request = Request("{0}/groups".format(api_server), data=json.dumps(new_group), headers=headers)
+# response_body = urlopen(request).read()
+# new_group_results = json.loads(response_body)
+# print json.dumps(new_group_results, sort_keys=True, indent=4, separators=(',', ': '))
+
+#
+# new_group_results
+#
+# {
+#     "data": {
+#         "automation_mode": null,
+#         "group_id": "2895682",
+#         "icon_id": null,
+#         "members": [],
+#         "multicast_ready": false,
+#         "name": "Group",
+#         "order": 0,
+#         "reading_aggregation": {}
+#     },
+#     "errors": [],
+#     "pagination": {}
+# }
+
+activate_group = {
+    "desired_state": {
+        "powered":"false"
+    }
+}
+
+headers = {"Authorization": "Bearer {0}".format(oauth2["data"]["access_token"])}
+request = Request("{0}/groups/2895682/activate".format(api_server), data=json.dumps(activate_group), headers=headers)
+response_body = urlopen(request).read()
+activate_group_results = json.loads(response_body)
+print json.dumps(activate_group_results, sort_keys=True, indent=4, separators=(',', ': '))
+
