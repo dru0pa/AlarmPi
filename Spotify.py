7dee4c79efa87b1d16ece2979f819e7e1f85dcce
Joel Roberts <joel@joelroberts.net>
2015-04-05 20:19:12 +0000

    Spotify bugs

--- Spotify.py
+++ Spotify.py
@@ -29,7 +29,7 @@ class Spotify:
             else:
                 self.audio_driver = spotify.PortAudioSink(self.session)
         except ImportError:
-            self.logger.warning(
+            log.warning(
                 'No audio sink found; audio playback unavailable.')
 
         self.config = spotify.Config()
@@ -57,7 +57,7 @@ class Spotify:
             self.session.relogin()
             self.logged_in.wait()
         except spotify.Error as e:
-            self.logger.error(e)
+            log.error(e)
 
     def forget_me(self, line):
         "forget_me -- forget the previous logged in user"
@@ -71,79 +71,79 @@ class Spotify:
     def whoami(self, line):
         "whoami"
         if self.logged_in.is_set():
-            self.logger.info(
+            log.info(
                 'I am %s aka %s. You can find me at %s',
                 self.session.user.canonical_name,
                 self.session.user.display_name,
                 self.session.user.link)
         else:
-            self.logger.info(
+            log.info(
                 'I am not logged in, but I may be %s',
                 self.session.remembered_user)
 
     def play_uri(self, line):
         "play <spotify track uri>"
         if not self.logged_in.is_set():
-            self.logger.warning('You must be logged in to play')
+            log.warning('You must be logged in to play')
             return
         try:
             track = self.session.get_track(line)
             track.load()
         except (ValueError, spotify.Error) as e:
-            self.logger.warning(e)
+            log.warning(e)
             return
-        self.logger.info('Loading track into player')
+        log.info('Loading track into player')
         self.session.player.load(track)
-        self.logger.info('Playing track')
+        log.info('Playing track')
         self.session.player.play()
 
     def pause(self, line):
-        self.logger.info('Pausing track')
+        log.info('Pausing track')
         self.session.player.play(False)
 
     def resume(self, line):
-        self.logger.info('Resuming track')
+        log.info('Resuming track')
         self.session.player.play()
 
     def stop(self, line):
-        self.logger.info('Stopping track')
+        log.info('Stopping track')
         self.session.player.play(False)
         self.session.player.unload()
 
     def seek(self, seconds):
         "seek <seconds>"
         if not self.logged_in.is_set():
-            self.logger.warning('You must be logged in to seek')
+            log.warning('You must be logged in to seek')
             return
         if self.session.player.state is spotify.PlayerState.UNLOADED:
-            self.logger.warning('A track must be loaded before seeking')
+            log.warning('A track must be loaded before seeking')
             return
         self.session.player.seek(int(seconds) * 1000)
 
     def search(self, query):
         "search <query>"
         if not self.logged_in.is_set():
-            self.logger.warning('You must be logged in to search')
+            log.warning('You must be logged in to search')
             return
         try:
             result = self.session.search(query)
             result.load()
         except spotify.Error as e:
-            self.logger.warning(e)
+            log.warning(e)
             return
-        self.logger.info(
+        log.info(
             '%d tracks, %d albums, %d artists, and %d playlists found.',
             result.track_total, result.album_total,
             result.artist_total, result.playlist_total)
-        self.logger.info('Top tracks:')
+        log.info('Top tracks:')
         for track in result.tracks:
-            self.logger.info(
+            log.info(
                 '[%s] %s - %s', track.link, track.artists[0].name, track.name)
 
     def get_playlists(self):
         for playlist in self.session.playlist_container:
             playlist_uri = playlist.load()
-            self.logger.info("Name: {0} URI: {1}".format(playlist.name, playlist_uri))
+            log.info("Name: {0} URI: {1}".format(playlist.name, playlist_uri))
 
 
 if __name__ == '__main__':
