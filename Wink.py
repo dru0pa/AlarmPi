7309298ea6055aeb4491887f3e8a76e1c960bb30
Joel Roberts <joel@joelroberts.net>
2015-04-05 03:15:12 +0000

    figure out how-to turn on/off a group

--- Wink.py
+++ Wink.py
@@ -3,7 +3,7 @@ import json
 import re
 
 api_server = "https://winkapi.quirky.com"
-api_server = "https://private-0d714-wink.apiary-mock.com"
+#api_server = "https://private-0d714-wink.apiary-mock.com"
 
 values = json.dumps({
     "client_id": "9575057b69283d441c17f3618d15d93d",
