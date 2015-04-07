b8332690528b8156939929f0ec22089fe7678f90
Joel Roberts <joel@joelroberts.net>
2015-04-07 23:57:48 +0000

    debug the debug web page

--- MediaPlayer.py
+++ MediaPlayer.py
@@ -31,7 +31,7 @@ class MediaPlayer:
 
     def playerActive(self):
         # log.debug("playerActive: {0}".format(self.player != False))
-        if self.player != False or self.spotify != False:
+        if self.player != False or self.spotify.spotify.session.player != False:
             return True
         else:
             return False
