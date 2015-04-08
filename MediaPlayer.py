07bf96dcb15988c1642696461d63f82be5078c61
Joel Roberts <joel@joelroberts.net>
2015-04-08 00:02:57 +0000

    debugging the cancel button. playerActive function needs to be updated for spotify

--- MediaPlayer.py
+++ MediaPlayer.py
@@ -31,7 +31,7 @@ class MediaPlayer:
 
     def playerActive(self):
         # log.debug("playerActive: {0}".format(self.player != False))
-        if self.player != False or self.spotify.spotify.session.player != False:
+        if self.player != False or self.spotify.spotify.session.player.state != "playing":
             return True
         else:
             return False
