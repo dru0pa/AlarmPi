ed0f8fb10a3ae1634137915f4bd10b21eaee7b08
Joel Roberts <joel@joelroberts.net>
2015-04-08 12:43:19 +0000

    adding individual threads to play, pause, resume in SpotifyThread

--- SpotifyThread.py
+++ SpotifyThread.py
@@ -42,15 +42,18 @@ class SpotifyThread(threading.Thread):
     def play(self):
         log.debug("play")
         #self.spotify.get_playlists()
-        self.spotify.play_playlist(self.settings.get("spotify_uri"))
+        #self.spotify.play_playlist(self.settings.get("spotify_uri"))
+        thr = threading.Thread(target=self.spotify.play_playlist, args=self.settings.get("spotify_uri"))
 
     def resume(self):
         log.debug("resume")
-        self.spotify.resume()
+        #self.spotify.resume()
+        thr = threading.Thread(target=self.spotify.resume())
 
     def pause(self):
         log.debug("pause")
-        self.spotify.pause()
+        #self.spotify.pause()
+        thr = threading.Thread(target=self.spotify.pause())
     #
     # def get_playlists(self):
     #     self.spotify.get_playlists()
